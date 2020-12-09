a9ac3ab210c6d9aeec87b2a9f2aa1911f6471c28
Menachem Neiman <Nerevar123@gmail.com>
2020-12-10 00:48:57 +0200

    fix: small fixes

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
